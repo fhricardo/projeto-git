ef1136e5744568ca47e002fedf762d4b4c68070f
Flavio Ricardo <fhricardo@gmail.com>
2025-05-15 14:43:30 -0300

    Update 20250515-1443: Code block line numbers | Code single line/inline style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 const year = document.querySelector('#year');
 const currentYear = new Date().getFullYear();
 year.textContent = currentYear;
+
+
+document.querySelectorAll('.code-block').forEach(block => {
+    const code = block.querySelector('code');
+    const lines = code.textContent.split('\n').length;
+    const lineNumbers = block.querySelector('.line-numbers');
+
+    // Gera os números de linha dinamicamente
+    lineNumbers.innerHTML = Array.from({ length: lines }, (_, i) => `<span>${i + 1}</span>`).join('');
+});
